098fc555fe77ca0185b8bdd3f42978ecb5763a28
vito <vitosgh@gmail.com>
2021-11-21 13:42:05 +0700

    Update tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -1,10 +1,9 @@
 import re
 
-def filter_unnecessary_tokens(tokens):
-    # remove string with zero length
-    result = filter(len, tokens)  
-    # remove whitespaces
-    whitespace = re.compile(r'[\t\r\v\f  ]+')
+def filter_unnecessary_tokens(tokens: list) -> list:
+    # remove string with zero length and remove whitespace
+    result = filter(len, tokens)
+    whitespace = re.compile(r'[\s]+')
     result = list(filter(lambda x: not whitespace.match(x), result))
     return result
 
@@ -18,9 +17,13 @@ def file_tokenizer(file_name: str) -> list:
         r'\+', r'\-', r'\*', '/', r'\*\*', '//', '%',                
         '<', '>', '<=', '>=', '==', '!=',
         r'\(', r'\)', r'\{', r'\}', ';', ':', ',', '=', '\'', '\"', 
-        '\n', ' '
+        '\n', ' ', '\t'
     ]
 
-    print(list(filter(filter_unnecessary_tokens, re.split('([' + r"".join(operators) + '])', contents))))
+    res = re.split('([' + r"".join(operators) + '])', contents)
 
-file_tokenizer("examples/inputAcc.py")
+    return filter_unnecessary_tokens(res)
+
+
+if __name__ == "__main__":
+    print(file_tokenizer("examples/inputAcc.py"))
