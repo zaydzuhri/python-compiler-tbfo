60eaa64f50fedaf91dcbf5ba0a703d96c83f037d
vito <vitosgh@gmail.com>
2021-11-22 11:38:46 +0700

    Change tokenizer to recognize newline

--- tokenizer.py
+++ tokenizer.py
@@ -3,20 +3,19 @@ import re
 def filter_unnecessary_tokens(tokens: list) -> list:
     # remove string with zero length and remove whitespace
     result = filter(len, tokens)
-    whitespace = re.compile(r'[\s]+')
+    whitespace = re.compile(r'[\r\t\v ]+')
     result = list(filter(lambda x: not whitespace.match(x), result))
     return result
 
 
 def file_tokenizer(file_name: str) -> list:
-    file = open(file_name, "r")
-    contents = file.read()
-    file.close()
+    with open(file_name, 'r') as f:
+        contents = f.read()
 
     delimiters = [
         r'\+', r'\-', r'\*', '/', r'\*\*', '//', '%', '<', '>', '<=', '>=',
         '==', '!=', r'\(', r'\)', r'\{', r'\}', ';', ':', ',', '=', '\'',
-        '\"', '<=', '>=', ':=', '\n', ' ', '\t'
+        '\"', ':=', '\n', ' ', '\t'
     ]
 
     res = re.split('([' + r"".join(delimiters) + '])', contents)
